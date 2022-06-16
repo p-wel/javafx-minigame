c6a000c384e9e3ac8291403a607514bd895dbeeb
p-wel <wojtowiczp97@gmail.com>
2022-06-16 21:47:31 +0200

    Wykłady

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module pl.pjatk.gui.pjatk_gui_pro_3 {
+module com.example.gui_pro_3 {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens pl.pjatk.gui.pjatk_gui_pro_3 to javafx.fxml;
-    exports pl.pjatk.gui.pjatk_gui_pro_3;
+    opens com.example.project_3 to javafx.fxml;
+    exports com.example.project_3;
 }
